44a801d35b09539b3970865e0e364992a40bef8d
aingianni92 <ingianni.anthony@gmail.com>
2022-10-25 19:03:32 -0400

    defined routes in route controller

---
+++ controllers/routeController.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const router = express.Router()
+const dataController = require('./dataController')
+const viewController = require('./viewController')
+
+// INDEX
+router.get('/', dataController.index, viewController.index)
+
+// NEW
+router.get('/new', viewController.newView)
+
+// DELETE
+router.delete('/:id', dataController.destroy, viewController.redirectHome)
+
+// UPDATE
+router.put('/:id', dataController.update, viewController.redirectShow)
+
+// CREATE
+router.post('/', dataController.create, viewController.edit)
+
+// EDIT
+router.get('/:id/edit', dataController.show, viewController.edit)
+
+// SHOW
+router.get('/:id', dataController.show, viewController.show)
+
+module.exports = router
