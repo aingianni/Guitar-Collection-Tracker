1ca04cf28d11c0fff010a3ccf18505994d93c31f
aingianni92 <ingianni.anthony@gmail.com>
2022-10-25 20:45:35 -0400

    created the show page

--- views/collections/Show.jsx
+++ views/collections/Show.jsx
@@ -3,9 +3,19 @@ const Default = require('../Layouts/Default')
 
 class Show extends React.Component {
   render () {
+    const { image, model, _id, color, price, bodyShape, guitarOwned } = this.props.collection
     return (
-      <Default>
-        
+      <Default title={`${model} View Page`} collection={this.props.collection}>
+        <img src={image} />
+        <br />
+        <ul>
+          <li> Model: {model}</li>
+          <li> Price: {price}</li>
+          <li> Color: {color}</li>
+          <li> Body Shape: {bodyShape}</li>
+        </ul>
+        <br />
+        <p>{guitarOwned? 'This guitar is in your collection' : 'This guitar is currently on your wishlist'}</p>
       </Default>
     )
   }
