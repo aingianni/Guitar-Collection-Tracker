42055b7c6395c5f0c53d7620ade3ca81446d03ca
aingianni92 <ingianni.anthony@gmail.com>
2022-10-25 20:39:41 -0400

    created Edit page in jsx

--- views/collections/Edit.jsx
+++ views/collections/Edit.jsx
@@ -3,9 +3,24 @@ const Default = require('../Layouts/Default')
 
 class Edit extends React.Component {
   render () {
+    const { image, model, _id, color, price, bodyShape, guitarOwned } = this.props.collection
     return (
-      <Default>
-        
+      <Default title={`${model} Edit Page`} collection={this.props.collection}>
+        <form method='POST' action={`/collections/${_id}?_method=PUT`}>
+          Image: <input name='image' defaultValue={image}></input>
+          <br />
+          Model: <input name='model' defaultValue={model}></input>
+          <br />
+          Price: <input name='price' defaultValue={price}></input>
+          <br />
+          Color: <input name='color' defaultValue={color}></input>
+          <br />
+          Body Shape: <input name='bodyShape' defaultValue={bodyShape}></input>
+          <br />
+          Acquired: <input type='checkbox' name='guitarOwned' defaultChecked={guitarOwned}></input>
+          <br />
+          <input type='submit' value='Update Guitar' />
+        </form>
       </Default>
     )
   }
--- views/collections/New.jsx
+++ views/collections/New.jsx
@@ -7,11 +7,17 @@ class New extends React.Component {
       <Default title="Add Guitar to Collection">
         <form method='POST' action='/collections'>
           Image: <input name='image' placeholder='Image Link for Guitar'></input>
+          <br />
           Model: <input name='model' placeholder='Model of the Guitar'></input>
+          <br />
           Price: <input name='price' placeholder='Price of the Guitar'></input>
+          <br />
           Color: <input name='color' placeholder='Color of the Guitar'></input>
+          <br />
           Body Shape: <input name='bodyShape' placeholder='Body Shape of the Guitar'></input>
+          <br />
           Acquired: <input type='checkbox' name='guitarOwned'></input>
+          <br />
           <input type='submit' value='Submit Guitar' />
         </form>
       </Default>
