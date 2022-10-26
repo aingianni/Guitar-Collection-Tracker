9d33bc099e9c49411e316d58a68553b420f3dc46
aingianni92 <ingianni.anthony@gmail.com>
2022-10-25 21:23:17 -0400

    created two tables on index page

--- views/collections/Index.jsx
+++ views/collections/Index.jsx
@@ -3,9 +3,58 @@ const Default = require('../Layouts/Default')
 
 class Index extends React.Component {
   render () {
+    const {collections} = this.props
+    const wishList = collections.filter(collection => collection.guitarOwned === false)
+    const collectionList = collections.filter(collection => collection.guitarOwned === true)
     return (
       <Default>
-        
+        <table>
+          {
+            collections.map(collection => {
+              const { image, model, _id, color, price, bodyShape, guitarOwned } = collection
+              return(
+                <tr key={collection._id}>
+                  <td>
+                    <a href={`/collections/${collection._id}`}>{model}</a>
+                    <br />
+                    {color}
+                    <br />
+                    <form method="POST" action={`/collections/${collection._id}?_method=DELETE`}>
+                      <input type="submit" value="Delete this Guitar" />
+                    </form>
+                  </td>
+                  <td>
+                    <img src={image} />
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </table>
+
+        <table>
+          {
+            collections.map(collection => {
+              const { image, model, _id, color, price, bodyShape, guitarOwned } = collection
+              return(
+                <tr key={collection._id}>
+                  <td>
+                    <a href={`/collections/${collection._id}`}>{model}</a>
+                    <br />
+                    {color}
+                    <br />
+                    <form method="POST" action={`/collections/${collection._id}?_method=DELETE`}>
+                      <input type="submit" value="Delete this Guitar" />
+                    </form>
+                  </td>
+                  <td>
+                    <img src={image} />
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </table>
       </Default>
     )
   }
